296b31d6765f23b791c5be7ed44f8616e826e0ae
levir-sousa <levysouza2014@gmail.com>
2022-08-04 16:43:19 -0300

    exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade")
+
+const nacionalidadesRegistradas = ["brasileira", "argentina", "uruguaia", "chilena", "colombiana"]
+
+if (nacionalidadesRegistradas.includes(nacionalidade)) {
+    console.log("Sua nacionalidade é:", nacionalidade);
+} else {
+    console.log("Nacionalidade não encontrada");
+}
